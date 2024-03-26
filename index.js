869e4eb8c948d3007f9f1e401fedfb7271646ec5
mpartificer <megan.forsey@hotmail.com>
2024-03-26 11:58:38 -0230

    Co-authored-by: Jillian Breau <jillianbreau@gmail.com>

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ const buttonLink = document.getElementById('letsGoButton');
 
 const linkToRecipe = document.getElementById('webpage');
 
-buttonLink.addEventListener('click', () => linkToRecipe.setAttribute('href', linkToRecipe.value))
+// const myLink = document.getElementById('myLink');
+
+buttonLink.addEventListener('click', () => {
+  // console.log("myLink", myLink)
+  // myLink.setAttribute('href', linkToRecipe.value)
+  console.log(linkToRecipe.value)
+  fetch('https://api.codetabs.com/v1/proxy?quest=https://www.allrecipes.com/recipe/261983/tourtiere-french-canadian-meat-pie/').then((response) => response.text()).then((text) => console.log(text));
+
+  
+})
