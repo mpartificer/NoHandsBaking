0912e7307034537e4d6df59464cba51fe784bbf8
mpartificer <megan.forsey@hotmail.com>
2024-03-28 15:48:10 -0230

    Testing commit

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 console.log("Hello!")
 
 const button = document.getElementById('test');
+const recipeTitle = document.getElementById('recipeTitle');
 
 button.addEventListener('click', () => button.setAttribute('class', 'my-blue-thing'))
 
 const buttonLink = document.getElementById('letsGoButton');
 
-const linkToRecipe = document.getElementById('webpage');
+function toggle(htmlID) {
+  if (htmlID.style.display === "none") {
+    htmlID.style.display = "block";
+  } else {
+    htmlID.style.display = "none";
+  }
+}
+
+async function retrieveWebString(){
+    const linkToRecipe = await document.getElementById('webpage');
+    finalLink = linkToRecipe.value;
+    return finalLink;
+};
 
 // const myLink = document.getElementById('myLink');
 
-buttonLink.addEventListener('click', () => {
+buttonLink.addEventListener('click', async () => {
   // console.log("myLink", myLink)
+
+  //Backend
+  const linkToRecipe = await retrieveWebString;
+  
+  
+  
+  //Frontend
+  toggle(recipeTitle);
+
+  //window.location = linkToRecipe;
   // myLink.setAttribute('href', linkToRecipe.value)
   console.log(linkToRecipe.value)
   fetch('https://api.codetabs.com/v1/proxy?quest=https://www.allrecipes.com/recipe/261983/tourtiere-french-canadian-meat-pie/').then((response) => response.text()).then((text) => console.log(text));
