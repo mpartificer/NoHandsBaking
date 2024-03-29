9ba971c8395f41f9150be77ceac8aa27cf75bc9e
mpartificer <megan.forsey@hotmail.com>
2024-03-29 16:17:43 -0230

    Tidied files and completed frameworks for all 3 stages

--- index.js
+++ index.js
@@ -1,51 +1,24 @@
 console.log("Hello!")
 
 const button = document.getElementById('test');
+const buttonLink = document.getElementById('letsGoButton');
 const recipeTitle = document.getElementById('recipeTitle');
 const miseEnPlaceSet = document.getElementById('lockedAndLoadedButton');
 const miseEnPlaceText = document.getElementById('miseEnPlace');
 const recipeSelector = document.getElementById('recipeSelector');
+const instructions = document.getElementById('instructions');
 
 button.addEventListener('click', () => button.setAttribute('class', 'my-blue-thing'))
 
-const buttonLink = document.getElementById('letsGoButton');
-
-function toggle(htmlID) {
-  if (htmlID.style.display === "none") {
-    htmlID.style.display = "block";
-  } else {
-    htmlID.style.display = "none";
-  }
-}
-
-async function retrieveWebString(){
-    const linkToRecipe = document.getElementById('webpage');
-    finalLink = linkToRecipe.value;
-    return finalLink;
-};
-
-// const myLink = document.getElementById('myLink');
-
 buttonLink.addEventListener('click', async () => {
-  // console.log("myLink", myLink)
 
-  //Backend
-  const linkToRecipe = await retrieveWebString();
   recipeTitle.setAttribute('class', 'recipeTitleVisible');
   miseEnPlaceText.setAttribute('class', 'miseEnPlaceVisible');
   recipeSelector.setAttribute('class', 'hideTheOpener');
-  
-  //Frontend
-  // toggle(recipeTitle);
-
-  //window.location = linkToRecipe;
-  // myLink.setAttribute('href', linkToRecipe.value)
-  console.log(linkToRecipe.value)
-  fetch('https://api.codetabs.com/v1/proxy?quest=https://www.allrecipes.com/recipe/261983/tourtiere-french-canadian-meat-pie/').then((response) => response.text()).then((text) => console.log(text));
-
-  
 })
 
-buttonLink.addEventListener('click', () => {
-  miseEnPlace.SetAttribute('class', 'miseEnPlace')
-})
+miseEnPlaceSet.addEventListener('click', () => {
+  instructions.setAttribute('class', 'instructionsVisible');
+  miseEnPlaceText.setAttribute('class', 'miseEnPlace');
+}
+)
