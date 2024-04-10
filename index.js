2e2f0875279f6933f9930fb99505aaf32602f096
mpartificer <megan.forsey@hotmail.com>
2024-04-10 18:48:31 -0230

    backbone of Spoonacular calls added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const miseEnPlaceSet = document.getElementById('lockedAndLoadedButton');
 const miseEnPlaceText = document.getElementById('miseEnPlace');
 const recipeSelector = document.getElementById('recipeSelector');
 const instructions = document.getElementById('instructions');
+const recipeSelectionScreen = document.getElementById('recipeSelectionScreen');
 const spoonacularKey = "3c5ec8b2939641a99e28c6023598b2d4";
 var i;
 
@@ -26,32 +27,67 @@ async function retrieveRecipes() {
   return recipesObj;
 }
 
-async function recipePreview() {
-
-}
-
-async function recipeSelect() {
-  const url = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${spoonacularKey}`
-
-}
-
-function listView(recipesObj) {
-  for (i = 0; i < recipesObj.length; i++) {
+function listView() {
+  for (i = 0; i < 10; i++) {
     elements[i].style.width = "100%";
   }
 }
 
-function gridView(recipesObj) {
-  for (i = 0; i < recipesObj.length; i++) {
+function gridView() {
+  for (i = 0; i < 10; i++) {
     elements[i].style.width = "50%";
   }
 }
 
+async function readMore(recipeID) {
+  const recipeSelect = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${spoonacularKey}`
+
+  const selectMyRecipe = await fetch(recipeSelect, {
+    headers: 
+      {"Content-Type": "application/json"
+  }});
+  
+  const recipesObj = await selectMyRecipe.json();
+  console.log(recipesObj);
+  console.log(recipesObj.results)
+
+  return recipesObj;
+}
+
+async function letsBake(recipeID) {
+  const recipeSelect = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${spoonacularKey}`
+
+  const selectMyRecipe = await fetch(recipeSelect, {
+    headers: 
+      {"Content-Type": "application/json"
+  }});
+  
+  const recipesObj = await selectMyRecipe.json();
+  console.log(recipesObj);
+  console.log(recipesObj.results)
+
+  return recipesObj;
+}
+
+async function recipeWaiter(recipesList) {
+  for (i = 0; i < recipesList.length; i++) {
+    document.GetElementById(`Recipe${i+1}`).innerHTML=recipesList[i].title;
+    const viewButton = document.getElementById(`recipe${i+1}View`)
+    const miseButton = document.getElementById(`recipe${i+1}Mise`)
+    const grabID = recipesList[i].id;
+    viewButton.value = grabID;
+    miseButton.value = grabID;
+  }
+}
+
 buttonLink.addEventListener('click', async () => {
-  const recipesList = retrieveRecipes();
+  const recipesList = await retrieveRecipes();
+  await recipeWaiter(recipesList);
+  
+  recipeSelectionScreen.setAttribute('class', 'recipeSelectionScreenVisible');
   // recipeTitle.setAttribute('class', 'recipeTitleVisible');
   // miseEnPlaceText.setAttribute('class', 'miseEnPlaceVisible');
-  // recipeSelector.setAttribute('class', 'hideTheOpener');
+  recipeSelector.setAttribute('class', 'hideTheOpener');
 })
 
 miseEnPlaceSet.addEventListener('click', () => {
