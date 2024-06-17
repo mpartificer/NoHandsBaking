bba4e035ff67a497a9fb269e33ea1c68b845e65b
mpartificer <megan.forsey@hotmail.com>
2024-06-17 16:54:04 -0230

    Scroll to Relevant Instruction Implemented

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ function previousFunction() {
     }
   }
 
+  scrollParentToChild(instructionsText, setAnimation);
   buttonCheck();
 
   console.log(currentInstruction)
@@ -175,10 +176,41 @@ function nextFunction() {
     }
   }
 
+  scrollParentToChild(instructionsText, setAnimation);
   buttonCheck();
 
-  
   waitYourTurn(setAnimation.innerText)
+}
+
+function scrollParentToChild(parent, child) {
+
+  // Where is the parent on page
+  var parentRect = parent.getBoundingClientRect();
+  // What can you see?
+  var parentViewableArea = {
+    height: parent.clientHeight,
+    width: parent.clientWidth
+  };
+
+  // Where is the child
+  var childRect = child.getBoundingClientRect();
+  // Is the child viewable?
+  var isViewable = (childRect.top >= parentRect.top) && (childRect.bottom <= parentRect.top + parentViewableArea.height);
+
+  // if you can't see the child try to scroll parent
+  if (!isViewable) {
+        // Should we scroll using top or bottom? Find the smaller ABS adjustment
+        const scrollTop = childRect.top - parentRect.top;
+        const scrollBot = childRect.bottom - parentRect.bottom;
+        if (Math.abs(scrollTop) < Math.abs(scrollBot)) {
+            // we're near the top of the list
+            parent.scrollTop += scrollTop;
+        } else {
+            // we're near the bottom of the list
+            parent.scrollTop += scrollBot;
+        }
+  }
+
 }
 
 function buttonCheck() {
